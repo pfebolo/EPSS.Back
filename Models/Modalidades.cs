e376185f68107fae49dbe4b5c24e46bb9f200da9
Pablo Alberto Febolo <pabloalberto.febolo@personal.com.ar>
2016-10-10 20:20:43 -0300

    Se configuro el connectionString
     En un archivo de configuración :)

--- Models/Modalidades.cs
+++ Models/Modalidades.cs
@@ -18,13 +18,23 @@ namespace WebCore.API.Models
         public DbSet<Modalidad> Modalidades { get; set; }
 
     
+        public IConfigurationRoot Configuration { get; set;}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
             //optionsBuilder.UseSqlite("Filename=../../../blog.db");
             //options.UseSqlServer(optionsBuilder.GetConnectionString("DefaultConnection"));
-            //Console.WriteLine(Configuration.GetConnectionString("DefaultConnection") );
-            optionsBuilder.UseSqlServer(@"Data Source=NB01\SQLEXPRESS;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=escuelapsdelsur;");
+            Console.WriteLine(Directory.GetCurrentDirectory() );
+            Console.WriteLine(Configuration.GetConnectionString("DefaultConnection") );
+            //optionsBuilder.UseSqlServer(@"Data Source=NB01\SQLEXPRESS;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=escuelapsdelsur;");
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             Console.WriteLine("Configuracion");
         }
         
