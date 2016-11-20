34181f00c9a766bd904f0dd2a4353c10b470a875
Pablo Alberto Febolo <pabloalberto.febolo@personal.com.ar>
2016-11-20 19:37:29 -0300

    * Se modifica Modelo Legajo   - Se felxibiliza el ingreso del domicio, permitiendo domicilios normalizados y no normalizados.

--- Models/Legajos.cs
+++ Models/Legajos.cs
@@ -26,8 +26,8 @@ namespace API.Models
         public string LugarNacimiento { get; set; }
         [Column("DNI")]
         public int Dni { get; set; }
-        [Column("CUIT")]
-        public int? Cuit { get; set; }
+        [Column("CUIT", TypeName = "decimal")]
+        public decimal? Cuit { get; set; }
         [Required]
         [MaxLength(255)]
         public string DireccionCalle { get; set; }
@@ -37,21 +37,42 @@ namespace API.Models
         [MaxLength(255)]
         public string DireccionCoordenadaInterna { get; set; }
         [Column("DireccionLocalidadID")]
-        public int DireccionLocalidadId { get; set; }
+        public int? DireccionLocalidadId { get; set; }
         [Column("DireccionPartidoID")]
-        public int DireccionPartidoId { get; set; }
-        [Required]
+        public int? DireccionPartidoId { get; set; }
         [Column("DireccionProvinciaID")]
         [MaxLength(50)]
         public string DireccionProvinciaId { get; set; }
-        [Required]
         [Column("DireccionPaisID")]
         [MaxLength(50)]
         public string DireccionPaisId { get; set; }
         [Column("SecundarioCompletoOLey25")]
-        public bool SecundarioCompletoOley25 { get; set; }
+        public bool? SecundarioCompletoOley25 { get; set; }
         [MaxLength(255)]
         public string Comentarios { get; set; }
+        public int? CodigoPostalBase { get; set; }
+        [MaxLength(255)]
+        public string LocalidadBase { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? FechaIngreso { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? PagoIniFecha { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string PagoIniMonto { get; set; }
+        [MaxLength(50)]
+        public string ModalidadBase { get; set; }
+        [MaxLength(50)]
+        public string Turno { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? Cuestionario { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DocAptoFisico { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DocTitulo { get; set; }
+        [Column("DocDNI", TypeName = "datetime")]
+        public DateTime? DocDni { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DocFoto { get; set; }
 
         [ForeignKey("AlumnoId")]
         [InverseProperty("Legajos")]
