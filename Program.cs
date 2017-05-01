11e3101a8a8beebb9138c79b44f4f6cef3462ac7
Pablo Fébolo <pfebolo@yahoo.com>
2017-05-01 20:26:05 -0300

    * Se ajusta Timer

--- Program.cs
+++ Program.cs
@@ -19,8 +19,8 @@ namespace EPSS
 
             ReglasDeInscripcion reglasDeInscripcion = new ReglasDeInscripcion();
             //TODO: Obtener los tiempos desde la configuración
-            //tmr = new Timer(reglasDeInscripcion.Cargar, null, 60000, 180000); //60000=10 Minutos-180000=30minutos
-            tmr = new Timer(reglasDeInscripcion.Cargar, null, 10000, Timeout.Infinite); 
+            tmr = new Timer(reglasDeInscripcion.Cargar, null, 60000, 180000); //60000=10 Minutos-180000=30minutos
+            //tmr = new Timer(reglasDeInscripcion.Cargar, null, 10000, Timeout.Infinite); 
             
 
             host.Run();
