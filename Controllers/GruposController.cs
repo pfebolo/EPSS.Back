836e1f4918f4770ced7df9b236159210d8b54147
Pablo Fébolo - WS01 <pfebolo@yahoo.com>
2017-10-17 20:59:43 -0300

    * Se agrega APIi DELETE /grupos

--- Controllers/GruposController.cs
+++ Controllers/GruposController.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using EPSS.Models;
 using EPSS.Repositories;
+using System;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace EPSS.Controllers
 {
@@ -44,10 +47,27 @@ namespace EPSS.Controllers
             return CreatedAtRoute("GetGrupos", new { controller = "Grupos", id = item.CarreraId }, item);
         }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            //_repo.Remove(id);
-        }
+		[HttpDelete("{CarreraId}/{ModoId}/{AnioInicio}/{MesInicio}/{AnioLectivo}/{NmestreLectivo}/{TurnoId}/{DivisionId}/{AlumnoId}")]
+		public IActionResult Delete(int CarreraId, string ModoId, int AnioInicio, int MesInicio, int AnioLectivo, int NmestreLectivo, string TurnoId, string DivisionId, int AlumnoId)
+		{
+			try
+			{
+				var item = _repo.Find(CarreraId, ModoId, AnioInicio, MesInicio, AnioLectivo, NmestreLectivo, TurnoId, DivisionId, AlumnoId);
+				if (item == null)
+				{
+					return NoContent(); //Sin error por que DELETE es Idempotente.
+				}
+				_repo.Remove(item); ;
+				return NoContent();
+			}
+			catch (Exception ex) when (ex is DbUpdateException || ex is DbUpdateConcurrencyException)
+			{
+				return Utils.ResponseConfict(ex);
+			}
+			catch (System.Exception ex)
+			{
+				return Utils.ResponseInternalError(ex);
+			}
+		}
     }
 }
