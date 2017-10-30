fe30bacc3b3d8ac6c821c16c408951f5b576379d
Pablo Fébolo - WS01 <pfebolo@yahoo.com>
2017-10-29 22:10:54 -0300

    * Se corrige alta de divisiones - Se elimina el create route

--- Controllers/DivisionesController.cs
+++ Controllers/DivisionesController.cs
@@ -8,11 +8,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EPSS.Controllers
 {
-    [Route("api/[controller]")]
-    public class DivisionesController : BaseController<Divisiones>
-    {
+	[Route("api/[controller]")]
+	public class DivisionesController : BaseController<Divisiones>
+	{
 
-		public DivisionesController(IRepository<Divisiones> repo) : base (repo){}
+		public DivisionesController(IRepository<Divisiones> repo) : base(repo) { }
 
 		[HttpGet("{CarreraId}/{ModoId}/{AnioInicio}/{MesInicio}/{AnioLectivo}/{NmestreLectivo}/{TurnoId}/{DivisionId}", Name = "GetDivisiones")]
 		public IActionResult GetById(int CarreraId, string ModoId, int AnioInicio, int MesInicio, int AnioLectivo, int NmestreLectivo, string TurnoId, string DivisionId)
@@ -25,16 +25,39 @@ namespace EPSS.Controllers
 			return new ObjectResult(item);
 		}
 
-        [HttpPost]
-        public override IActionResult Create([FromBody] Divisiones item)
-        {
-            if (item == null)
-            {
-                return BadRequest();
-            }
-            _repo.Add(item);
-            return CreatedAtRoute("GetDivisiones", new { controller = "Divisiones", id = item.CarreraId }, item);
-        }
+		[HttpPost]
+		public override IActionResult Create([FromBody] Divisiones item)
+		{
+			try
+			{
+				if (item == null)
+				{
+					return BadRequest();
+				}
+				_repo.Add(item);
+				// return CreatedAtRoute("GetDivisiones", new
+				// {
+				// 	controller = "Divisiones",
+				// 	CarreraId = item.CarreraId,
+				// 	ModoId = item.ModoId,
+				// 	AnioInicio = item.AnioInicio,
+				// 	MesInicio = item.MesInicio,
+				// 	AnioLectivo = item.AnioLectivo,
+				// 	NmestreLectvo = item.NmestreLectivo,
+				// 	TurnoId = item.TurnoId,
+				// 	DivisionId = item.DivisionId,
+				// }, item);
+				return Utils.ResponseCreated(); //No devuelve la ruta
+			}
+			catch (Exception ex) when (ex is DbUpdateException || ex is DbUpdateConcurrencyException)
+			{
+				return Utils.ResponseConfict(ex);
+			}
+			catch (Exception ex)
+			{
+				return Utils.ResponseInternalError(ex);
+			}
+		}
 
 		// PUT api/Divisiones
 		[HttpPut]
@@ -45,7 +68,7 @@ namespace EPSS.Controllers
 				if (item == null)
 					return BadRequest();
 
-				var division = _repo.Find(item.CarreraId,item.ModoId,item.AnioInicio,item.MesInicio,item.AnioLectivo,item.NmestreLectivo,item.TurnoId,item.DivisionId);
+				var division = _repo.Find(item.CarreraId, item.ModoId, item.AnioInicio, item.MesInicio, item.AnioLectivo, item.NmestreLectivo, item.TurnoId, item.DivisionId);
 
 				if (division == null)
 					return NotFound();
@@ -67,7 +90,7 @@ namespace EPSS.Controllers
 		[HttpDelete("{CarreraId}/{ModoId}/{AnioInicio}/{MesInicio}/{AnioLectivo}/{NmestreLectivo}/{TurnoId}/{DivisionId}")]
 		public IActionResult Delete(int CarreraId, string ModoId, int AnioInicio, int MesInicio, int AnioLectivo, int NmestreLectivo, string TurnoId, string DivisionId)
 		{
-			return base.Delete(new Object[] {CarreraId,ModoId,AnioInicio,MesInicio,AnioLectivo,NmestreLectivo,TurnoId,DivisionId});
+			return base.Delete(new Object[] { CarreraId, ModoId, AnioInicio, MesInicio, AnioLectivo, NmestreLectivo, TurnoId, DivisionId });
 		}
-    }
+	}
 }
