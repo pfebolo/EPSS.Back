ca45b8701b3aff1e40dcce1d3160ce80d3422106
Pablo Alberto Febolo - NB01 <pfebolo@yahoo.com>
2017-12-05 21:36:08 -0300

    Se crea marca de seguimiento de estudiantes

---
+++ DBScripts/DBBackUp.sql
@@ -0,0 +1,4 @@
+BACKUP DATABASE [escuelapsdelsur] TO  
+DISK = N'.\escuelapsdelsur20171205.bak' WITH NOFORMAT, 
+NOINIT,  NAME = N'escuelapsdelsur-Completa Base de datos Copia de seguridad', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
