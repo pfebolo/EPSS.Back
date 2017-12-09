4760788ae91aa7b605c68ab9ccf152ca1b6ababd
Pablo Fébolo - WS01 <pfebolo@yahoo.com>
2017-12-09 20:32:09 -0300

    * Se agrega API GET grupos/ByAlumno - Se Extiende la interface IRepository para permitir metodos adicionales a los genéricos

--- Repositories/GruposRepository.cs
+++ Repositories/GruposRepository.cs
@@ -3,12 +3,18 @@ using System;
 using EPSS.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 
 
 namespace EPSS.Repositories
 {
-    public class GruposRepository: BaseRepositoryNew<Grupos>
+    //Extiende la Interfaz Génerica con métodos especificos del Modelo
+    public interface IGruposRepository: IRepository<Grupos>
+    {
+        IEnumerable<Grupos> FindByAlumnoId(int alumnoId);
+    }
+    public class GruposRepository: BaseRepositoryNew<Grupos>, IGruposRepository
     {
 
         public GruposRepository(ILoggerFactory loggerFactory) : base (loggerFactory){}
@@ -37,6 +43,32 @@ namespace EPSS.Repositories
           }
           return _list.AsReadOnly();
         }
+		public IEnumerable<Grupos> FindByAlumnoId(int AlumnoId)
+		{
+			List<Grupos> buscados = new List<Grupos>();
+			try
+			{
+				using (var db = new escuelapsdelsurContext())
 
+				{
+					foreach (var grupo in from gr in db.Grupos.Include(Grupo => Grupo.Division)
+													        .ThenInclude(Division => Division.Curso)
+														    .ThenInclude(Curso => Curso.Carrera)
+													 where gr.AlumnoId == AlumnoId
+													 select gr)
+					{
+						buscados.Add(grupo);
+					}
+					_logger.LogInformation("Buscar Grupos x AlumnoId " + AlumnoId.ToString() + ", cantidad:" + buscados.Count().ToString() + " --> OK");
+				}
+			}
+			catch (System.Exception ex)
+			{
+				_logger.LogError(ex.Message);
+				throw ex;
+			}
+			return buscados.AsReadOnly();
+
+		}
     }
 }
