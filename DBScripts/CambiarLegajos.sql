42054969569ae4fdb1efcfcd772179dbf20b26b1
Pablo Fébolo - WS01 <pfebolo@yahoo.com>
2017-12-10 19:17:01 -0300

    .* Se ajustan los conceptos de Estado del Estudiante - Se fusionan estados Inscripto y Cursando en Activo. - El Estado Activo se lo considera el estado por omisión del Estudiante.

--- DBScripts/CambiarLegajos.sql
+++ DBScripts/CambiarLegajos.sql
@@ -1,13 +1,18 @@
 :r UseDb.sql
 ALTER TABLE dbo.Legajos ADD
-	Seguimiento bit NOT NULL CONSTRAINT DF_Legajos_Seguimiento DEFAULT 0
+	Seguimiento bit NOT NULL CONSTRAINT DF_Legajos_Seguimiento DEFAULT 0,
+	RazonSuspension nvarchar(255) NULL
 GO
 DECLARE @v sql_variant 
 SET @v = N'Marca que indica que el estudiante tiene algún pendiente'
 EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Legajos', N'COLUMN', N'Seguimiento'
 GO
+DECLARE @v sql_variant 
+SET @v = N'Razón por lo cual se suspendio al estudiante.'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Legajos', N'COLUMN', N'RazonSuspension'
+GO
 ALTER TABLE dbo.Legajos ADD
-	EstadoEstudianteID nvarchar(255) NOT NULL CONSTRAINT DF_Legajos_EstadoEstudianteID DEFAULT N'Inscripto'
+	EstadoEstudianteID nvarchar(255) NOT NULL CONSTRAINT DF_Legajos_EstadoEstudianteID DEFAULT N'Activo'
 GO
 DECLARE @v sql_variant 
 SET @v = N'Estado del Estudiante'
--- DBScripts/InsertarDatos.sql
+++ DBScripts/InsertarDatos.sql
@@ -1,11 +1,9 @@
 :r UseDb.sql
 BEGIN TRAN
 -- Dispositivos
-Insert into EstadosEstudiante Values('Inscripto',0);
-Insert into EstadosEstudiante Values('Cursando',1);
+Insert into EstadosEstudiante Values('Activo',1);
 Insert into EstadosEstudiante Values('Egresado',0);
-Insert into EstadosEstudiante Values('Suspendido por pago atrasado',0);
-Insert into EstadosEstudiante Values('Suspendido por razón académica.',0);
+Insert into EstadosEstudiante Values('Suspendido',0);
 Insert into EstadosEstudiante Values('Abandonó',0);
 Insert into EstadosEstudiante Values('Expulsado',0);
 COMMIT
